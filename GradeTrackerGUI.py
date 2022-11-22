f9fd3dccf75d4bd154b6cf775554b137185c7ab3
danieldiresta <111393948+danieldiresta@users.noreply.github.com>
2022-11-21 20:41:53 -0500

    Update GradeTrackerGUI.py
    -reformatted and organized the way the buttons and input should go

--- GradeTrackerGUI.py
+++ GradeTrackerGUI.py
@@ -7,14 +7,15 @@ root = tk.Tk()
 
 root.geometry("650x650")
 result_text = tk.Text(root, height=3, width=16, font=("Arial", 24))
-e = Entry(root, width=40, bg="green")
-e.pack()
-
 def click():
     hello = "Grade is " + e.get()
     Labels= Label(root, text=hello)
     Labels.pack()
 Buttons = Button(root, text="Enter the Grade to Python Module 1", command=click)
 Buttons.pack()
+e = Entry(root, width=40, bg="green")
+e.pack()
+
+
 
 root.mainloop()
