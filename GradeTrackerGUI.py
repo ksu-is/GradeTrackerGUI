9634c8a62b5c3d64ee5eff71e56a35b159bfd30c
danieldiresta <111393948+danieldiresta@users.noreply.github.com>
2022-11-21 20:04:30 -0500

    Add files via upload

---
+++ GradeTrackerGUI.py
@@ -0,0 +1,20 @@
+from urllib import request
+from tkinter import *
+
+import tkinter as tk
+
+root = tk.Tk()
+
+root.geometry("650x650")
+result_text = tk.Text(root, height=3, width=16, font=("Arial", 24))
+e = Entry(root, width=40, bg="green")
+e.pack()
+
+def click():
+    hello = "Grade is " + e.get()
+    Labels= Label(root, text=hello)
+    Labels.pack()
+Buttons = Button(root, text="Enter the Grade to Python Module 1", command=click)
+Buttons.pack()
+
+root.mainloop()
