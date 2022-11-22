c180c96aeebe4096ce0c5b4fe7b2226a4e85a1c1
danieldiresta <111393948+danieldiresta@users.noreply.github.com>
2022-11-21 21:12:10 -0500

    Update GradeTrackerGUI.py
    -created frames and messed around with how I wanted to set up the text boxes

--- GradeTrackerGUI.py
+++ GradeTrackerGUI.py
@@ -4,6 +4,11 @@ from tkinter import *
 import tkinter as tk
 
 root = tk.Tk()
+window = tk.Tk()
+window.title("Grade Tracker")
+
+frame = tk.Frame(window)
+frame.pack()
 
 root.geometry("650x650")
 result_text = tk.Text(root, height=3, width=16, font=("Arial", 24))
@@ -11,11 +16,14 @@ def click():
     hello = "Grade is " + e.get()
     Labels= Label(root, text=hello)
     Labels.pack()
-Buttons = Button(root, text="Enter the Grade to Python Module 1", command=click)
-Buttons.pack()
 e = Entry(root, width=40, bg="green")
-e.pack()
 
+Buttons1 = Button(root, text="Enter the Grade to Python Absolute Beginner Module 1", command=click)
+Buttons2 = Button(root, text="Enter the Grade to Python Absolute Beginner Module 2", command=click)
+Buttons1.pack()
+e.pack()
+Buttons2.pack()
+e.pack()
 
 
 root.mainloop()
